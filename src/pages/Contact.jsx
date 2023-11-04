a388501c870e6452c6325a6d1e7dd7714e52baa2
Cyrille Asima <casima@terpmail.umd.edu>
2023-11-04 18:04:24 -0400

    update

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+
+
+const Contact = () => {
+
+    const [message, setMessage] = useState('')
+    const [landlord, setLandlord] = useState(null)
+    // eslint-disable-next-line
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const params = useParams()
+
+    useEffect (() => {
+        const getLandlord = async () => {
+            const docRef = doc(db, 'users', params.landlordId)
+            const docSnap = await getDoc(docRef)
+
+            if (docSnap.exists()) {
+                setLandlord(docSnap.data())
+            } else {
+                toast.error('Could not get Owner data')
+            }
+            }
+
+        getLandlord()
+        }, [params.landlordId])
+
+
+
+
+
+    return (
+        <div className='pageContainer'>
+            <header>
+                <p className='pageHeader'> Contact Owner</p>
+            </header>
+
+            {landlord !== null && (
+                <main>
+                    <div className='contactLandlord'>
+                        <p className='landlordName'>Contact {landlord?.name}</p>
+                    </div>
+                </main>
+            )}
+        </div>
+    )
+}
+
+export default Contact
